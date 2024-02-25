9beb1d57bc5925f63e33b0f36839d99f6deae0b7
Jayanth-Kulkarni <kulka112@uwindsor.ca>
2024-02-24 18:53:48 -0800

    alternate solution for palindrome

--- two_pointers/valid_palindrome.py
+++ two_pointers/valid_palindrome.py
@@ -4,3 +4,21 @@ s = "A man, a plan, a canal: Panama"
 s = s.lower().replace(" ", "")
 s = re.sub(r'\W+', '', s)
 return s == s[::-1]
+
+
+# alternate solution
+
+def is_palindrome(s):
+  left = 0
+  right = len(s)-1
+  if s[left]!=s[right]:
+      return False
+  left +=1
+  right -=1
+  while left < right:
+      print(s[left],s[right])
+      if s[left]!=s[right]:
+          return False
+      left +=1
+      right -=1
+  return True
