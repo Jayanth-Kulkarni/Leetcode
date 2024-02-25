7ba3e3a7aee15e494a7e16ee19f6951db317e475
Jayanth-Kulkarni <kulka112@uwindsor.ca>
2024-02-24 19:08:13 -0800

    updated alternate solution for palindrome

--- two_pointers/valid_palindrome.py
+++ two_pointers/valid_palindrome.py
@@ -7,18 +7,24 @@ return s == s[::-1]
 
 
 # alternate solution
+def isPalindrome(self, s: str) -> bool:
+    s = s.lower().replace(" ","")
+    s = re.sub(r'\W|_', '', s)
+    print(s)
+    if len(s)==0:
+        return True
+    left = 0
+    right = len(s)-1
+    print(left,right)
+    if s[left]!=s[right]:
+        return False
+    left +=1
+    right -=1
+    while left < right:
+        print(s[left],s[right])
+        if s[left]!=s[right]:
+            return False
+        left +=1
+        right -=1
+    return True
 
-def is_palindrome(s):
-  left = 0
-  right = len(s)-1
-  if s[left]!=s[right]:
-      return False
-  left +=1
-  right -=1
-  while left < right:
-      print(s[left],s[right])
-      if s[left]!=s[right]:
-          return False
-      left +=1
-      right -=1
-  return True
