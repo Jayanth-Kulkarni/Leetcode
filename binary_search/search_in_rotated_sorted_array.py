b6c8a143df48a4a1d7520fbf21e86e5ee8098f86
Jayanth-Kulkarni <kulka112@uwindsor.ca>
2024-04-10 22:08:45 -0700

    fixed binary_search_rotated

--- binary_search/search_in_rotated_sorted_array.py
+++ binary_search/search_in_rotated_sorted_array.py
@@ -1,20 +1,24 @@
 class Solution:
-    def findMin(self, nums) -> int:
-        l = 0
-        res = nums[0]
-        r = len(nums)-1
-        left_val,right_val = nums[l],nums[r]
-        while l<=r:
-            if nums[l]<nums[r]:
-                res = min(res,nums[l])
-                break
-            mid = int((l+r)/2)
-            res = min(res,nums[mid])
-            if nums[mid] >= left_val:
-                l = mid+1
-            else:
-                r = mid-1
-        return res
+	def binary_search_rotated(nums, target):
+		low = 0
+		high = len(nums) - 1
+
+		while low <= high:
+			mid = low + (high - low) // 2
+			if nums[mid] == target:
+				return mid
+
+			if nums[low] <= nums[mid]:
+				if nums[low] <= target and target < nums[mid]:
+					high = mid - 1 
+				else:
+					low = mid + 1 
+			else:
+				if nums[mid] < target and target <= nums[high]:
+					low = mid + 1
+				else:
+					high = mid - 1 
+		return -1
 
 
 if __name__ == "__main__":
