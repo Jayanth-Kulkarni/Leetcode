fe74696de4dfa446cccc2dee3c2646283a749a35
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2024-12-29 14:34:20 -0800

    Time: 125 ms (28.67%), Space: 26.9 MB (15.79%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -3,6 +3,6 @@ class Solution:
         min_price = math.pow(10,5) + 1
         max_profit = -1
         for price in prices:
-            min_price = min(price,min_price)
+            min_price = min(min_price,price)
             max_profit = max(max_profit, price-min_price)
         return max_profit
