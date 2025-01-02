64f49bfcb6e9c93aa95cb16a83a803df5ff51cca
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-01-02 12:24:24 -0800

    Time: 146 ms (58.44%), Space: 19.7 MB (74.83%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,21 +1,22 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        row, col = len(mat), len(mat[0])
-        move = [(0,1),(0,-1),(1,0),(-1,0)]
+        row, column = len(mat), len(mat[0])
         queue = deque()
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
         for i in range(row):
-            for j in range(col):
+            for j in range(column):
                 if mat[i][j] == 0:
                     queue.append([i,j])
                 else:
                     mat[i][j] = float("inf")
-
+        
         while len(queue) > 0:
-            i, j = queue.popleft()
-            for dir_i, dir_j in move:
-                new_i, new_j = i + dir_i, j +  dir_j
-                if 0 <= new_i < row and 0 <= new_j < col and mat[new_i][new_j] > mat[i][j] + 1:
-                    mat[new_i][new_j] = mat[i][j] + 1
-                    queue.append([new_i,new_j])
+            r, c =  queue.popleft()
+            for i, j in directions:
+                new_r, new_c =  r+i, c+j
+                if 0<= new_r < row and 0<= new_c < column and mat[new_r][new_c] > mat[r][c] + 1:
+                    mat[new_r][new_c] = mat[r][c] + 1
+                    queue.append([new_r, new_c])
 
         return mat
