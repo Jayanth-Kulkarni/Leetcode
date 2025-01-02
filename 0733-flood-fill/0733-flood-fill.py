019d4161993f1fa7308f6f9c264b1a8a3ec25207
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-01-01 17:15:27 -0800

    Time: 0 ms (100%), Space: 17.8 MB (20.78%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,26 +1,25 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        queue = []
         original_color = image[sr][sc]
-        queue.append([sr,sc])
+        queue = [[sr,sc]]
         image[sr][sc] = color
         visited = [[sr,sc]]
         while len(queue) > 0:
-            sr,sc = queue.pop() 
-            if sr-1>=0 and [sr-1,sc] not in visited and image[sr-1][sc] == original_color:
-                visited.append([sr-1,sc])
-                queue.append([sr-1,sc])
+            sr,sc = queue.pop()
+            if sr-1 >= 0 and [sr-1,sc] not in visited and image[sr-1][sc] == original_color:
                 image[sr-1][sc] = color
-            if sr+1<len(image) and [sr+1,sc] not in visited and  image[sr+1][sc] == original_color:
-                visited.append([sr+1,sc])
-                queue.append([sr+1,sc])
+                queue.append([sr-1,sc])
+                visited.append([sr-1,sc])
+            if sr+1 < len(image) and [sr+1,sc] not in visited and image[sr+1][sc] == original_color:
                 image[sr+1][sc] = color
-            if sc-1>=0 and [sr,sc-1] not in visited and image[sr][sc-1] == original_color:
-                visited.append([sr,sc-1])
-                queue.append([sr,sc-1])
+                queue.append([sr+1,sc])
+                visited.append([sr+1,sc])
+            if sc-1 >= 0 and [sr,sc-1] not in visited and image[sr][sc-1] == original_color:
                 image[sr][sc-1] = color
-            if sc+1<len(image[0]) and [sr,sc+1] not in visited and image[sr][sc+1] == original_color:
-                visited.append([sr,sc+1])
-                queue.append([sr,sc+1])
+                queue.append([sr,sc-1])
+                visited.append([sr,sc-1])
+            if sc+1 < len(image[0]) and [sr,sc+1] not in visited and  image[sr][sc+1] == original_color:
                 image[sr][sc+1] = color
+                queue.append([sr,sc+1])
+                visited.append([sr,sc+1])
         return image
