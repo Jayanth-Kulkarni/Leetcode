a9ad96e468a17c3da8d6dbe7ad6e8062ac8084f7
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-01-01 17:22:50 -0800

    Time: 2 ms (99.17%), Space: 17.9 MB (43.3%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,8 +1,7 @@
 class Solution:
-    import string
     def isPalindrome(self, s: str) -> bool:
+        import string
         for punct in string.punctuation:
-            if punct in s:
-                s = s.replace(punct,"")
+            s = s.replace(punct,"")
         s = s.replace(" ","").lower()
-        return s == s[::-1]
+        return s==s[::-1]
