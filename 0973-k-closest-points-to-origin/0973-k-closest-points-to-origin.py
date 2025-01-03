df6e51d8203267c7e0d79f3debbc1612738deaf6
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-01-02 17:42:01 -0800

    Time: 63 ms (67.11%), Space: 22.8 MB (16.39%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -3,6 +3,7 @@ class Solution:
         distances = []
         for point in points:
             distances.append([math.pow(point[0],2)+math.pow(point[1],2),point])
-        sorted_distances = sorted(distances, key= lambda x: x[0])
-        result = [i[1] for i in sorted_distances[:k]]
-        return result
+        
+        sorted_distances = sorted(distances, key=lambda x:x[0])[:k]
+
+        return [distance[1] for distance in sorted_distances]
