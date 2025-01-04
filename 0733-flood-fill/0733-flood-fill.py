fc5728eee43853e56db99f0472eceb02742080da
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-01-04 10:49:54 -0800

    Time: 3 ms (18.54%), Space: 17.9 MB (17.47%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,25 +1,19 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        visited = []
+        queue = deque()
+        row, col = len(image), len(image[0])
         original_color = image[sr][sc]
-        queue = [[sr,sc]]
         image[sr][sc] = color
-        visited = [[sr,sc]]
+        visited.append([sr,sc])
+        queue.append([sr,sc])
+        directions = ((0,1),(0,-1),(1,0),(-1,0))
         while len(queue) > 0:
-            sr,sc = queue.pop()
-            if sr-1 >= 0 and [sr-1,sc] not in visited and image[sr-1][sc] == original_color:
-                image[sr-1][sc] = color
-                queue.append([sr-1,sc])
-                visited.append([sr-1,sc])
-            if sr+1 < len(image) and [sr+1,sc] not in visited and image[sr+1][sc] == original_color:
-                image[sr+1][sc] = color
-                queue.append([sr+1,sc])
-                visited.append([sr+1,sc])
-            if sc-1 >= 0 and [sr,sc-1] not in visited and image[sr][sc-1] == original_color:
-                image[sr][sc-1] = color
-                queue.append([sr,sc-1])
-                visited.append([sr,sc-1])
-            if sc+1 < len(image[0]) and [sr,sc+1] not in visited and  image[sr][sc+1] == original_color:
-                image[sr][sc+1] = color
-                queue.append([sr,sc+1])
-                visited.append([sr,sc+1])
+            sr, sc = queue.popleft()
+            for direction in directions:
+                new_sr, new_sc = sr + direction[0], sc + direction[1]
+                if 0 <= new_sr < row and 0 <= new_sc < col and [new_sr, new_sc] not in visited and image[new_sr][new_sc] == original_color:
+                    image[new_sr][new_sc] = color
+                    queue.append([new_sr, new_sc])
+                    visited.append([new_sr, new_sc])
         return image
