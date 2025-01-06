8361583523fd69ac7a6b37fd38c1260398eed2dc
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-01-06 15:14:54 -0800

    Time: 1193 ms (16.08%), Space: 20.8 MB (22.77%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,23 +1,26 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # sort the numbers, and skip the repeated values for first and second positions
-        nums =  sorted(nums)
+        # sort the array
+        nums = sorted(nums)
         result = []
+
+        # skip the repeats for first and second value in array
         for i in range(len(nums)):
             if i > 0 and nums[i-1] == nums[i]:
                 continue
             first = nums[i]
-            l = i+1 
+            l = i+1
             r = len(nums)-1
             while l < r < len(nums):
-                three_sum = first + nums[l] + nums[r]
-                if l < r < len(nums) and three_sum > 0:
-                    r -= 1
-                elif l < r < len(nums) and three_sum < 0:
-                    l += 1
-                elif three_sum == 0:
+                threesum = first + nums[l] + nums[r]
+                if threesum == 0:
                     result.append([first, nums[l], nums[r]])
                     l += 1
-                    while l < r < len(nums) and nums[l-1] == nums[l]: 
+                    while l < r < len(nums) and nums[l-1] == nums[l]:
                         l += 1
+                elif l < r < len(nums) and threesum > 0:
+                    r -= 1
+                elif l < r < len(nums) and threesum < 0:
+                    l += 1
         return result
+
