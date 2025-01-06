e3fb94180542867ad2d84a5575959a4ffca1869f
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-01-06 14:05:29 -0800

    Time: 0 ms (100%), Space: 19.6 MB (40.41%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,15 +1,14 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        # Run a for loop, for that item check if newInterval needs to be before or after the item
-        # Place the item in that position, in case of overlap -> take the min or first index and max of second index, add it to result and return
         result = []
         for i in range(len(intervals)):
-            if newInterval[0] > intervals[i][1]:
-                result.append(intervals[i])
-            elif newInterval[1] < intervals[i][0]:
+            if newInterval[1] < intervals[i][0]:
                 result.append(newInterval)
                 return result + intervals[i:]
+            elif newInterval[0] > intervals[i][1]:
+                result.append(intervals[i])
             else:
-                newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
+                newInterval = [min(intervals[i][0], newInterval[0]), max(intervals[i][1], newInterval[1])]
+
         result.append(newInterval)
         return result
