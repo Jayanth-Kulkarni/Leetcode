a747690a8726d555bc892b6caff0b6b5fb1725aa
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-01-08 07:57:59 -0800

    Time: 136 ms (18.55%), Space: 26.9 MB (28.66%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,9 +1,14 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        # save max_profit as -1, take the min_val as the first element
+        # run a loop over all the prices and find the difference between the min_val and price
+        # and calculate the max of the 2. Return max after the loop
+
         max_profit = -1
-        min_val = math.pow(10,4)+1
+        min_val = prices[0]
         for price in prices:
-            min_val = min(price,min_val)
-            max_profit = max(max_profit,price-min_val)
+            profit = price - min_val
+            min_val = min(min_val, price)
+            max_profit = max(max_profit, profit)
+        
         return max_profit
-            
