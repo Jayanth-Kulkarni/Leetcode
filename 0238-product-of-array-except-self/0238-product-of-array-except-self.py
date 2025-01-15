98230e94b09b86584efdcfe20a44449c4475d1a0
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-01-14 18:01:55 -0800

    Time: 72 ms (5.09%), Space: 28.1 MB (7.41%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,29 +1,26 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # calculate preprod, and postprod
-        # [a, b, c, d]
-        # preprod = [a, ab, abc, abcd]
-        # postprod = [abcd, bcd, cd, d]
-        # result = [bcd, acd, abd, abc]
-        preprod = [nums[0]]
-        for num in nums[1:]:
-            preprod.append(preprod[-1] * num)
-        
-        reversed_nums = nums[::-1]
-        postprod = [reversed_nums[0]]
-        for num in reversed_nums[1:]:
-            postprod.append(postprod[-1] * num)
-
-        postprod = postprod[::-1]
-        
         result = []
-
-        for i in range(len(nums)):
-            if i>0 and i<len(nums)-1:
-                result.append(preprod[i-1] * postprod[i+1])
-            elif i>0:
-                result.append(preprod[i-1])
-            elif i<len(nums)-1:
-                result.append(postprod[i+1])
         
+        # preprod
+        preprod = [nums[0]]
+        for idx, i in enumerate(nums[1:]):
+            preprod.append(preprod[-1] * i)
+        
+        rev = nums[::-1]
+        print(rev)
+        postprod = [rev[0]]
+        for idx, i in enumerate(rev[1:]):
+            postprod.append(postprod[-1] * i)
+        
+        postprod = postprod[::-1]
+
+        print(preprod,postprod)
+        for i in range(len(nums)):
+            if i-1>=0 and i+1<len(nums):
+                result.append(preprod[i-1] * postprod[i+1])
+            elif i-1>=0: 
+                result.append(preprod[i-1])
+            elif i+1<len(nums):
+                result.append(postprod[i+1])
         return result
