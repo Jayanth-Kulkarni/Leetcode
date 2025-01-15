e4c148d6cb728d3f3f4b57db7ed4289a58af288e
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-01-15 10:18:46 -0800

    Time: 8 ms (27.77%), Space: 19.5 MB (12.67%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,42 +1,33 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # form the adj list
-        map = {course:[] for course in range(numCourses)}
-        
-        # fill the adj list
-        for course, prereq in prerequisites:
-            map[course].append(prereq)
+        # Create the adj list for the graph
 
-        # visited set to detect loops
+        adj = {course:[] for course in range(numCourses)}
+        for course, prereq in prerequisites:
+            adj[course].append(prereq)
+
+        # to find the loop
         visited = set()
 
-        # dfs to identify loops
         def dfs(course):
-
-            # if there are no prereqs, that course can be completed
-            if map[course] == []:
+            if adj[course] == []:
                 return True
-
-            # if a loop is found return False
+            
             if course in visited:
                 return False
 
-            # Add the current course to the path (incompleted visited!)
             visited.add(course)
+            for prereq in adj[course]:
+                if not dfs(prereq):
+                    return False
 
-            # Run loop-checker for all the prereq for that course
-            for prereq in map[course]:
-                if not dfs(prereq): return False 
-
-            # Remove the course from the visited set, as it's completed
             visited.remove(course)
-
-            # Remove all prereqs in the map
-            map[course] = []
+            adj[course] = []
 
             return True
-
+        
         for course in range(numCourses):
-            if not dfs(course): return False
-
+            if not dfs(course):
+                return False
+        
         return True
