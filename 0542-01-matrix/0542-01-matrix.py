5c1a7cb531c1fac4fa9bb173e0b3d408b7429cc7
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-01-16 13:45:53 -0800

    Time: 136 ms (62.55%), Space: 19.6 MB (78.65%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,24 +1,21 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        r, c = len(mat), len(mat[0])
-        directions = [(0,1), (0,-1), (1,0), (-1,0)]
-        # Refill the non-zero cells to be inf
-
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         queue = deque()
-        for row in range(r):
-            for col in range(c):
-                if mat[row][col] == 0:
-                    queue.append((row,col))
+        row = len(mat)
+        col = len(mat[0])
+        for r in range(row):
+            for c in range(col):
+                if mat[r][c] == 0:
+                    queue.append((r, c))
                 else:
-                    mat[row][col] = float("inf")
-        # Loop through the queue and perform DFS and update the values
+                    mat[r][c] = float("inf")
         
         while len(queue) > 0:
             r, c = queue.popleft()
-            for nr, nc in directions:
-                new_r, new_c = r+nr, c+nc
-                if 0 <= new_r < len(mat) and 0 <= new_c < len(mat[0]) and mat[new_r][new_c] > mat[r][c] + 1:
-                    mat[new_r][new_c] = mat[r][c] + 1
-                    queue.append((new_r, new_c))
-        
+            for r_, c_ in directions:
+                nr, nc = r + r_, c + c_
+                if 0 <= nr < row and 0 <= nc < col and mat[nr][nc] > 1 + mat[r][c]:
+                    mat[nr][nc] = 1 + mat[r][c]
+                    queue.append((nr, nc))
         return mat
