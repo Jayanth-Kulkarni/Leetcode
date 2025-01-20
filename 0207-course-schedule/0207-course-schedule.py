c429d1298bde236015dba1672dfd8ea636daacfe
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-01-19 18:13:34 -0800

    Time: 0 ms (100%), Space: 19.5 MB (14.65%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,36 +1,28 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # Create the adj list for the graph
-        map = {courses: [] for courses in range(numCourses)}
+        map = {i:[] for i in range(numCourses)}
 
-        # Fill the map with prereqs
-        for course, prereq in  prerequisites:
-            map[course].append(prereq)
-
-        # Create a visited set to check for loops
         visited = set()
 
-        # Perform DFS using this function
+        for course, prereq in prerequisites:
+            map[course].append(prereq)
+        
         def dfs(course):
 
-            # if already visited (loop) return False
             if course in visited:
                 return False
             
-            # If all the prereqs are complated return True
             if map[course] == []:
                 return True
-                
-            visited.add(course)            
-            # Loop through all the prereq and make sure it can be completed
+            
+            visited.add(course)
+            
             for prereq in map[course]:
                 if not dfs(prereq):
                     return False
-            
-            # Remove the course from visited list as it can be completed
+
             visited.remove(course)
 
-            # Mark the prereqs for this course as empty, as it can be completed
             map[course] = []
 
             return True
@@ -40,6 +32,3 @@ class Solution:
                 return False
 
         return True
-
-            
-
