6d7e84fd18abb4af5195906b5e0fb1b056f3da77
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-01-19 18:40:31 -0800

    Time: 7 ms (29.02%), Space: 17.6 MB (55.99%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,35 +1,31 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        # mark all the 2s to the queue
+        queue =  deque()
+        row, col = len(grid), len(grid[0])
+        for i in range(row):
+            for j in range(col):
+                if grid[i][j] == 2:
+                    queue.append((i, j))
+        iterations = 0
         visited = set()
-        queue = deque()
-        directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
-        nr, nc = len(grid), len(grid[0])
-        time = 0
-
-        # Add all initially rotten oranges to the queue
-        for row in range(nr):
-            for col in range(nc):
-                if grid[row][col] == 2:
-                    queue.append((row, col))
-                    visited.add((row, col))
+        while len(queue) > 0:
+            for i in range(len(queue)):
+                r,c = queue.popleft()
+                for dir in [(1, 0), (-1, 0), (0, -1), (0, 1)]:
+                    n_r = r + dir[0]
+                    n_c = c + dir[1]
+                    if 0 <= n_r < row and 0 <= n_c < col and grid[n_r][n_c] == 1 and (n_r, n_c) not in visited:
+                        grid[n_r][n_c] = 2
+                        queue.append((n_r, n_c))
+                        visited.add((n_r, n_c))
+            if len(queue) > 0:
+                iterations += 1
         
-        # BFS to spread rot
-        while queue:
-            for _ in range(len(queue)):  # Process current level
-                r_, c_ = queue.popleft()
-                for dr, dc in directions:
-                    r, c = r_ + dr, c_ + dc
-                    if 0 <= r < nr and 0 <= c < nc and grid[r][c] == 1 and (r, c) not in visited:
-                        grid[r][c] = 2
-                        visited.add((r, c))
-                        queue.append((r, c))
-            if queue:  # Only increment time if there are oranges being processed
-                time += 1
+        complete = True
+        for i in range(row):
+            for j in range(col):
+                if grid[i][j] == 1:
+                    complete = False
 
-        # Check if there are any fresh oranges left
-        for row in range(nr):
-            for col in range(nc):
-                if grid[row][col] == 1:
-                    return -1
-
-        return time
+        return iterations if complete else -1
