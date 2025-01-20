f8d40d2c7b040372ae0ab907318979f598979f9d
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-01-19 18:48:09 -0800

    Time: 136 ms (62.42%), Space: 19.7 MB (79.39%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,22 +1,21 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        # Perform BFS, collect all the cells that are marked 0, mark other cells inf
         row, col = len(mat), len(mat[0])
         queue = deque()
-        directions = [(1, 0), (-1, 0), (0, -1), (0, 1)]
         for i in range(row):
             for j in range(col):
                 if mat[i][j] == 0:
-                    queue.append((i,j)) 
+                    queue.append((i, j))
                 else:
                     mat[i][j] = float("inf")
         
         while len(queue) > 0:
             r, c = queue.popleft()
-            for r_, c_ in directions:
-                nr, nc = r + r_, c + c_
-                if 0 <= nr < row and 0 <= nc < col and mat[nr][nc] > mat[r][c] + 1:
-                    mat[nr][nc] = mat[r][c] + 1
-                    queue.append((nr, nc))
+            for dir in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                n_r = dir[0] + r
+                n_c = dir[1] + c
+                if 0 <= n_r < row and 0 <= n_c < col and mat[n_r][n_c] > 1 + mat[r][c]:
+                    mat[n_r][n_c] = 1 + mat[r][c]
+                    queue.append((n_r, n_c))
         
         return mat
