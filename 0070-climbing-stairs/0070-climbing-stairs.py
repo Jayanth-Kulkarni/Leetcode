60c37812a6ca74ccf3696ad28d7830a47f059738
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-01-19 18:51:23 -0800

    Time: 0 ms (100%), Space: 17.8 MB (28.26%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,8 +1,8 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        res = [0 for i in range(n+1)]
+        res = [float("inf") for i in range(n+1)]
         res[n] = 1
         res[n-1] = 1
-        for i in reversed(range(0,n-1)):
+        for i in reversed(range(0, n-1)):
             res[i] = res[i+1] + res[i+2]
         return res[0]
