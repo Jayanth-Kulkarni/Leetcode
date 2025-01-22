f6d7ad3d796ead3e9eed2b849482b2b2c38d007f
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-01-21 16:01:27 -0800

    Time: 0 ms (100%), Space: 19.8 MB (42.13%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -9,10 +9,10 @@ class Solution:
         def dfs(root, left, right):
             if not root:
                 return True
-
+            
             if not (left < root.val < right):
                 return False
-            
-            return dfs(root.left, left, root.val) and dfs(root.right, root.val, right)
-        
+
+            return (dfs(root.left, left, root.val) and dfs(root.right, root.val, right))
+
         return dfs(root, float("-inf"), float("inf"))
