5b3b98209c9247eaa66beb640a81ba84fb66b28f
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-01-24 12:34:31 -0800

    Time: 6 ms (9.7%), Space: 19.7 MB (29.24%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,19 +1,14 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         result = []
-        for i in range(len(intervals)):
-            # if new interval is before the current interval, insert new interval to result
-            if newInterval[1] < intervals[i][0]:
+        for idx, interval in enumerate(intervals):
+            if interval[1] < newInterval[0]:
+                result.append(interval)
+            elif newInterval[1] < interval[0]:
                 result.append(newInterval)
-                return result + intervals[i:]
-            # if new interval is after the current interval, insert current interval to result
-            elif newInterval[0] >  intervals[i][1]:
-                result.append(intervals[i])
-            # else create a new interval by resizing the window
+                return result + intervals[idx:]
             else:
-                newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
-        
-        # append the newInterval to result
+                newInterval = [min(interval[0], newInterval[0]), max(interval[1], newInterval[1])]
         result.append(newInterval)
     
         return result
