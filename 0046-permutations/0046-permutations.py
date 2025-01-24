0b36f4dd51222c590fe3075a04fb60493207d268
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-01-24 11:49:47 -0800

    Time: 0 ms (100%), Space: 17.8 MB (62.18%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -5,9 +5,9 @@ class Solution:
         
         perms = self.permute(nums[1:])
         res = []
-        for p in perms:
-            for i in range(len(p)+1):
-                p_copy = p.copy()
-                p_copy.insert(i, nums[0])
-                res.append(p_copy)
+        for perm in perms:
+            for i in range(len(perm)+1):
+                copy = perm.copy()
+                copy.insert(i, nums[0])
+                res.append(copy)
         return res
