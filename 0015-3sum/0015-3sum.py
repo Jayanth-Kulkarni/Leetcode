21fbd219ed74447e501f166aaf4268e2c333f07b
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-01-26 22:14:34 -0800

    Time: 1107 ms (17.25%), Space: 20.8 MB (32.83%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -3,19 +3,19 @@ class Solution:
         nums = sorted(nums)
         result = []
         for i in range(len(nums)):
-            if i > 0 and nums[i-1] == nums[i]:
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
-            f = nums[i]
-            l = i + 1
-            r = len(nums) - 1
-            while l < r < len(nums):
-                s, t = nums[l], nums[r]
-                if f + s + t == 0:
-                    result.append([f, s, t])
+            first = nums[i]
+            l = i+1
+            r = len(nums)-1
+            while 0 < l < r < len(nums):
+                second, third = nums[l], nums[r]
+                if first + second + third == 0:
+                    result.append([first, second, third])
                     l += 1
-                    while 0 < l < r and nums[l-1] == nums[l]:
+                    while 0 < l < r < len(nums) and nums[l-1] == nums[l]:
                         l += 1
-                elif f + s + t > 0:
+                elif first + second + third > 0:
                     r -= 1
                 else:
                     l += 1
