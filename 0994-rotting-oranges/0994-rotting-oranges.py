f6df02c02872903e0b73158aaca063d88ea5c837
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-01-30 16:17:32 -0800

    Time: 3 ms (83.49%), Space: 17.6 MB (73.96%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -2,30 +2,27 @@ class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         queue = deque()
         row, col = len(grid), len(grid[0])
-        visited = set()
-        iteration = 0
         for r in range(row):
             for c in range(col):
                 if grid[r][c] == 2:
-                    queue.append((r, c))
-                    visited.add((r, c))
-
-        while len(queue) > 0:
-            for _ in range(len(queue)):
-                r_, c_ = queue.popleft()
-                for ro, co in [(1,0), (-1, 0), (0, 1), (0, -1)]:
-                    r, c = ro + r_, co + c_
-                    if 0 <= r < row and 0 <= c < col and (r, c) not in visited and grid[r][c] == 1:
-                        grid[r][c] = 2
-                        queue.append((r, c))
-                        visited.add((r, c))
-            if len(queue) > 0:
-                iteration += 1
+                    queue.append((r,c))
         
-        complete = True
+        iterations = -1
+        while len(queue):
+            length = len(queue)
+            for _ in range(length):
+                ro, co = queue.popleft()
+                for r_, c_ in [(1,0), (-1,0), (0,1), (0,-1)]:
+                    r, c = ro+r_, co+c_
+                    if 0 <= r < row and 0 <= c < col and grid[r][c] == 1:
+                        queue.append((r,c))
+                        grid[r][c] = 2
+            iterations += 1
+            length = len(queue)
+
         for r in range(row):
             for c in range(col):
                 if grid[r][c] == 1:
-                    complete = False
-                    break
-        return iteration if complete else -1
+                    return -1
+
+        return iterations if iterations > -1 else 0
