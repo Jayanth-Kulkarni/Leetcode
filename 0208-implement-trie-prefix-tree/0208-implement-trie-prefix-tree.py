1898d4c339ad403d2531bc637debb236cb38e6dd
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-01-31 14:13:49 -0800

    Time: 59 ms (33.76%), Space: 32.2 MB (50.21%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -1,37 +1,37 @@
 class TrieNode:
     def __init__(self):
-        self.children = {}
+        self.trie = {}
         self.eos = False
+
 class Trie:
+
     def __init__(self):
-        self.trie = TrieNode()        
+        self.prefix = TrieNode()
 
     def insert(self, word: str) -> None:
-        trie = self.trie
-        for letter in word:
-            if letter not in trie.children:
-                trie.children[letter] = TrieNode()                
-            trie = trie.children[letter]
+        trie = self.prefix
+        for s in word:
+            if s not in trie.trie:
+                trie.trie[s] = TrieNode()
+            trie = trie.trie[s]
         trie.eos = True
-        return
+
 
     def search(self, word: str) -> bool:
-        trie = self.trie
-        for letter in word:
-            if letter in trie.children:
-                trie = trie.children[letter]
-            else:
-                return False 
+        trie = self.prefix
+        for s in word:
+            if s not in trie.trie:
+                return False
+            trie = trie.trie[s]
         return trie.eos
 
     def startsWith(self, prefix: str) -> bool:
-        trie = self.trie
-        for letter in prefix:
-            if letter in trie.children:
-                trie = trie.children[letter]
-            else:
-                return False 
-        return True
+        trie = self.prefix
+        for s in prefix:
+            if s not in trie.trie:
+                return False
+            trie = trie.trie[s]
+        return True       
 
 
 # Your Trie object will be instantiated and called as such:
