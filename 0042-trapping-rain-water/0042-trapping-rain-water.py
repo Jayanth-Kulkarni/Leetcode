9817a7037c4b467545a31aaf659c5369d813bfc0
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-02-02 18:17:18 -0800

    Time: 33 ms (16.86%), Space: 19.7 MB (5.67%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,19 +1,13 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        # find the min from left 
-        # find the min from right
-        minLeft, minRight = [0], [0]
-        for i in height:
-            minLeft.append(max(minLeft[-1], i))
-        
-        for i in height[::-1]:
-            minRight.append(max(minRight[-1], i))
-
-        minRight = minRight[::-1]
-
-        res_val = 0
-        for l, r, h in zip(minLeft, minRight, height):
-            if min(l, r) - h  > 0:
-                res_val += min(l, r) - h
-        
-        return res_val
+        leftmax, rightmax = [0], [0]
+        for h in height:
+            leftmax.append(max(leftmax[-1], h))
+        for h in height[::-1]:
+            rightmax.append(max(rightmax[-1], h))
+        rightmax = rightmax[::-1]
+        res = 0
+        for l, r, h in zip(leftmax, rightmax, height):
+            if min(l,r) - h > 0:
+                res += min(l,r) - h
+        return res
