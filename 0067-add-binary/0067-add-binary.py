c84498fa540acbd7e96cef70830814cbdafb910a
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-02-04 11:38:01 -0800

    Time: 4 ms (18.44%), Space: 17.9 MB (44.84%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -3,16 +3,16 @@ class Solution:
         a, b = a[::-1], b[::-1]
         carry = 0
         res = []
+        for i in range(max(len(a),len(b))):
+            digita = int(a[i]) if i < len(a) else 0
+            digitb = int(b[i]) if i < len(b) else 0
 
-        for i in range(max(len(a), len(b))):
-            digit1 = int(a[i]) if i < len(a) else 0
-            digit2 = int(b[i]) if i < len(b) else 0
-            
-            sum = (digit1 + digit2 + carry) % 2
-            carry = (digit1 + digit2 + carry) // 2
+            total = (digita + digitb + carry)
+            sum = total % 2
+            carry = total // 2
             res.append(str(sum))
         
         if carry:
             res.append("1")
-        
+    
         return "".join(res[::-1])
