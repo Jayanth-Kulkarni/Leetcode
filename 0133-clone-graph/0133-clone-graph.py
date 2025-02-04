9bc63d0a3e2d7fba7ebe6d2e86971356c6848c7c
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-02-04 12:47:20 -0800

    Time: 39 ms (79.72%), Space: 18 MB (68.86%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -10,10 +10,9 @@ from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         clone = {}
-
         def dfs(node):
             if not node:
-                return None 
+                return None
             if node in clone:
                 return clone[node]
             
@@ -22,7 +21,6 @@ class Solution:
 
             for nei in node.neighbors:
                 clone[node].neighbors.append(dfs(nei))
-
+            
             return clone[node]
-        
         return dfs(node)
