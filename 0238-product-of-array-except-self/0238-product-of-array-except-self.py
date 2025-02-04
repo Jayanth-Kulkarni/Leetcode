64c0b3e8ffd662f09774e512d8e13925fc61d6fb
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-02-04 11:54:18 -0800

    Time: 43 ms (13.49%), Space: 28 MB (5.32%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,27 +1,27 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        pre_prod = [nums[0]]
-        for i in nums[1:]:
-            pre_prod.append(i * pre_prod[-1])
-        
-        reverse = nums[::-1]
-        post_prod = [reverse[0]]
-        for i in reverse[1:]:
-            post_prod.append(i * post_prod[-1])
-        
-        res = []
+        # [a,b,c,d]
+        # [a,ab,abc,abcd]
+        # [abcd,bcd,cd,d]
+        # [bcd,acd,abd,abc]
 
-        # [a, b, c, d]
-        # pre_prod = [a, ab, abc, abcd]
-        # post_prod = [abcd, dcb, dc, d]
-        # res = [dcb, adc, abd, abc]
-        post_prod = post_prod[::-1]
-        for i in range(len(nums)):
-            if i-1 >=0 and i+1 < len(nums):
-                res.append(pre_prod[i-1] * post_prod[i+1])
-            elif  i-1 >= 0:
-                res.append(pre_prod[i-1])
-            elif  i+1 < len(nums):
-                res.append(post_prod[i+1])
+        preprod = [nums[0]]
+        for num in nums[1:]:
+            preprod.append(preprod[-1] * num)
         
+        postprod = [nums[-1]]
+        rev = nums[::-1]
+        for num in rev[1:]:
+            postprod.append(postprod[-1] * num)
+        
+        postprod = postprod[::-1]
+
+        res = []
+        for i in range(len(preprod)):
+            if 0 <= i-1 < len(preprod) and 0 <= i+1 < len(preprod):
+                res.append(preprod[i-1] * postprod[i+1])
+            elif 0 <= i-1 < len(preprod):
+                res.append(preprod[i-1])
+            elif 0 <= i+1 < len(preprod):
+                res.append(postprod[i+1])
         return res
