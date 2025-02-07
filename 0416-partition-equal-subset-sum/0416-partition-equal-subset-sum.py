00d605d2522f333e1add06de74f2534603f97a2a
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-02-07 09:28:26 -0800

    Time: 316 ms (83.58%), Space: 19.3 MB (58.15%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -2,15 +2,16 @@ class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         if sum(nums) % 2:
             return False
+        
         dp = set()
         dp.add(0)
         target = sum(nums)/2
         for num in nums:
             copy = set()
-            for i in dp:
-                copy.add(i + num)
-                copy.add(i)
-                if target in dp:
-                    return True
+            for e in dp:
+                copy.add(num + e)
+                copy.add(e)
             dp = copy
+            if target in dp:
+                return True
         return False
