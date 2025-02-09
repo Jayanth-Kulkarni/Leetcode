965e26e20f882e51662e092520b4a467c8a021ed
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-02-09 13:31:29 -0800

    Time: 7 ms (49.77%), Space: 19.5 MB (21.86%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,6 +1,6 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        map = {course:[] for course in range(numCourses)}
+        map = {i:[] for i in range(numCourses)}
 
         for course, prereq in prerequisites:
             map[course].append(prereq)
@@ -10,24 +10,25 @@ class Solution:
         def dfs(course):
             if course in visited:
                 return False
-            
+
             if map[course] == []:
                 return True
             
             visited.add(course)
 
             for prereq in map[course]:
-                if dfs(prereq) == False:
-                    return False 
-
-            visited.remove(course)
+                if not dfs(prereq):
+                    return False
             
+            visited.remove(course)
+
             map[course] = []
-
+            
             return True
+        
 
-        for course in map:
+        for course in range(numCourses):
             if not dfs(course):
-                return False 
-
+                return False
+        
         return True
