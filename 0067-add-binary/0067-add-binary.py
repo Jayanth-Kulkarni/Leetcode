671ae68c756d87afa9a5416c8dd3a5eccdec27e6
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-02-09 18:20:08 -0800

    Time: 3 ms (45.77%), Space: 17.9 MB (48.14%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,18 +1,18 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         a, b = a[::-1], b[::-1]
+        res = ""
         carry = 0
-        res = []
-        for i in range(max(len(a),len(b))):
+        for i in range(max(len(a), len(b))):
             digita = int(a[i]) if i < len(a) else 0
             digitb = int(b[i]) if i < len(b) else 0
 
-            total = (digita + digitb + carry)
-            sum = total % 2
-            carry = total // 2
-            res.append(str(sum))
+            sum = (digita + digitb + carry) % 2
+            carry = int((digita + digitb + carry) / 2)
+
+            res += str(sum)
         
         if carry:
-            res.append("1")
-    
-        return "".join(res[::-1])
+            res += "1"
+        
+        return res[::-1]
