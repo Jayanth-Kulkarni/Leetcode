523dfe0c29c3df5aab9ab9211e14eaa8c37e9e95
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-02-10 10:09:57 -0800

    Time: 131 ms (67.85%), Space: 19.7 MB (84.08%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -4,7 +4,7 @@ class Solution:
         queue = deque()
         for r in range(row):
             for c in range(col):
-                if mat[r][c] != 0:
+                if mat[r][c] == 1:
                     mat[r][c] = float("inf")
                 else:
                     queue.append((r,c))
@@ -12,7 +12,8 @@ class Solution:
         while len(queue) > 0:
             ro, co = queue.popleft()
             for r_, c_ in [(1,0), (-1,0), (0,1), (0,-1)]:
-                r, c = ro + r_, co + c_
+                r = ro + r_
+                c = co + c_
                 if 0 <= r < row and 0 <= c < col and mat[r][c] > mat[ro][co] + 1:
                     mat[r][c] = mat[ro][co] + 1
                     queue.append((r,c))
