5ce03236458e8f4525863a33a5f81fd376667300
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-02-11 20:57:45 -0800

    Time: 58 ms (89.44%), Space: 18.3 MB (52.21%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,35 +1,35 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         if t == "": return ""
-
-        countT, window = {}, {}
-        for c in t:
-            countT[c] = 1 + countT.get(c, 0)
-        
-        have, need = 0, len(countT)
-        res, resLen = [-1, -1], float("inf")
         l = 0
-        for r in range(len(s)):
-            c = s[r]
-            window[c] = 1 + window.get(c, 0)
 
-            if c in countT and window[c] == countT[c]:
+        td, window = {}, {}
+        res, resLen = [-1,-1], float("inf")
+        for i in t:
+            td[i] = 1 + td.get(i, 0)
+        have, want = 0, len(td)
+        for r in range(len(s)):
+            i = s[r]
+            window[i] = 1 + window.get(i, 0)
+            
+            if i in td and window[i] == td[i]:
                 have += 1
             
-            while need == have:
+            while have == want:
+                j = s[l]
                 if r-l+1 < resLen:
                     resLen = r-l+1
                     res = [l, r]
-                d = s[l]
-                window[d] -= 1
 
-                if d in countT and window[d] < countT[d]:
-                    have -= 1
-                l += 1
-        
-        l, r = res
-        if resLen != float("inf"):
-            return s[l:r+1]
-
-        return ""
+                window[j] -= 1
                 
+                if j in td and window[j] < td[j]:
+                    have -= 1                
+                l += 1
+
+        if resLen == float("inf"):
+            return ""
+
+        l, r = res
+        return s[l:r+1]
+            
