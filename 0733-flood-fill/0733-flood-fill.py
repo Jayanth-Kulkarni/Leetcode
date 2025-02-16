c3c230740bb84fc025b1483fc5e2d7f1136d6356
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-02-16 11:12:30 -0800

    Time: 0 ms (100%), Space: 17.9 MB (88.44%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,19 +1,17 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        # collect the rows and cols that are = 1 in the queue
-        queue = deque()
         row, col = len(image), len(image[0])
-        original_color = image[sr][sc]
-        image[sr][sc] = color
+        queue = deque()
         queue.append((sr, sc))
-        visited = set()
-        while len(queue) > 0:
-            ro, co =  queue.popleft()
-            for r_, c_ in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-                r, c = ro + r_, co + c_
-                if 0 <= r < row and 0 <= c < col and image[r][c] == original_color and (r, c) not in visited:
+        oc = image[sr][sc]
+        image[sr][sc] = color
+        visited = set((sr,sc))
+        while queue:
+            r1, c1 = queue.popleft()
+            for r_, c_ in [(1,0), (-1, 0), (0,1), (0,-1)]:
+                r, c = r1 + r_, c1 + c_
+                if 0 <= r < row and 0 <= c < col and image[r][c] == oc and (r,c) not in visited:
                     image[r][c] = color
-                    queue.append((r, c))
-                    visited.add((r,c))
-        
+                    queue.append((r,c))
+                    visited.add((r,c)) 
         return image
