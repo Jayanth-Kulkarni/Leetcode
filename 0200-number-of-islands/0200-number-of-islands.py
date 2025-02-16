c81760551afeed3424eb39f24c2b39d93e054792
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-02-16 10:58:02 -0800

    Time: 253 ms (52.2%), Space: 25.6 MB (18.3%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,26 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        
         row, col = len(grid), len(grid[0])
         island = 0
-
-        def dfs(ro,co):
+        visited = set()
+        def dfs(ro, co):
             queue = deque()
-            queue.append((ro,co))
-            while len(queue) > 0:
-                ro, co = queue.popleft()
-                for r_, c_ in [(1,0), (-1,0), (0,1), (0,-1)]:
-                    r = ro + r_
-                    c = co + c_
-                    if 0 <= r < row and 0 <= c < col and grid[r][c] == "1":
-                        grid[r][c] = "2"
-                        queue.append((r,c))                    
+            queue.append((ro, co))
+            visited.add((ro,co))
+            while queue:
+                r_, c_ = queue.popleft()
+                for r1, c1 in [(1,0), (-1,0), (0,1), (0,-1)]:
+                    r = r1 + r_
+                    c = c1 + c_
+                    if 0 <= r < row and 0 <= c < col and grid[r][c] == "1" and (r,c) not in visited:
+                        visited.add((r,c))
+                        queue.append((r,c))
 
-
-        for r in range(row):
-            for c in range(col):
-                if grid[r][c] == "1":
-                    dfs(r,c)
+        for i in range(row):
+            for j in range(col):
+                if grid[i][j] == "1" and (i,j) not in visited:
+                    dfs(i,j)
                     island += 1
         
         return island
