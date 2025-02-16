9ef34e158846f084ca0291aaff004a85ba42ecf8
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-02-16 11:34:22 -0800

    Time: 6 ms (32.6%), Space: 17.9 MB (45.37%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -2,35 +2,28 @@ class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         row, col = len(grid), len(grid[0])
         queue = deque()
-        for r in range(row):
-            for c in range(col):
-                if grid[r][c] == 2:
-                    queue.append((r,c))
+        res = 0
+        for i in range(row):
+            for j in range(col):
+                if grid[i][j] == 2:
+                    queue.append((i, j))
         
-
-        count = 0
-        while len(queue) > 0:
+        while queue:
             level = len(queue)
             for i in range(level):
-                ro, co = queue.popleft()
-                for r_, c_ in [(1,0), (-1,0), (0,1), (0,-1)]:
-                    r = ro + r_
-                    c = co + c_
+                r1, c1 = queue.popleft()
+                for r_, c_ in [(1,0), (-1,0), (0, 1), (0, -1)]:
+                    r = r1 + r_
+                    c = c1 + c_
                     if 0 <= r < row and 0 <= c < col and grid[r][c] == 1:
                         grid[r][c] = 2
-                        queue.append((r,c))
+                        queue.append((r,c))      
+            if len(queue):
+                res += 1
+        
+        for i in range(row):
+            for j in range(col):
+                if grid[i][j] == 1:
+                    return -1
+        return res
             
-            if len(queue) > 0:
-                count += 1
-
-        flag = 0
-        for r in range(row):
-            for c in range(col):
-                if grid[r][c] == 1:
-                    flag = 1
-                    break
-        
-        if flag:
-            return -1
-        
-        return count
