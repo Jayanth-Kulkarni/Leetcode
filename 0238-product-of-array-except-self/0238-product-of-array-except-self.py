a6718948a3110b4284280f90b69eaa96ca2da687
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-02-17 13:32:40 -0800

    Time: 40 ms (15.34%), Space: 28.8 MB (5.7%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,27 +1,23 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # [a,b,c,d]
-        # [a,ab,abc,abcd]
-        # [abcd,bcd,cd,d]
-        # [bcd,acd,abd,abc]
-
         preprod = [nums[0]]
-        for num in nums[1:]:
-            preprod.append(preprod[-1] * num)
-        
         postprod = [nums[-1]]
-        rev = nums[::-1]
-        for num in rev[1:]:
-            postprod.append(postprod[-1] * num)
-        
-        postprod = postprod[::-1]
 
+        for i in nums[1:]:
+            preprod.append(preprod[-1] * i)
+
+        nums = nums[::-1]
+        for i in nums[1:]:
+            postprod.append(postprod[-1] * i)
+        
+        postprod, nums = postprod[::-1], nums[::-1]
         res = []
         for i in range(len(preprod)):
-            if 0 <= i-1 < len(preprod) and 0 <= i+1 < len(preprod):
+            if i > 0 and i < len(preprod)-1:
                 res.append(preprod[i-1] * postprod[i+1])
-            elif 0 <= i-1 < len(preprod):
+            elif i > 0:
                 res.append(preprod[i-1])
-            elif 0 <= i+1 < len(preprod):
+            elif i < len(preprod):
                 res.append(postprod[i+1])
+        
         return res
