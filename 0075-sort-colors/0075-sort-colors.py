d58a441923ae8247dc3dbec5185f9a4da65c9d42
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-02-17 19:49:35 -0800

    Time: 0 ms (100%), Space: 17.9 MB (11.97%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,12 +3,12 @@ class Solution:
         red, white, blue = 0, 0, len(nums)-1
         while white <= blue:
             if nums[white] == 0:
-                nums[red], nums[white] = nums[white], nums[red]
-                red += 1
+                nums[white], nums[red] = nums[red], nums[white]
                 white += 1
+                red += 1
             elif nums[white] == 1:
                 white += 1
             elif nums[white] == 2:
-                nums[blue], nums[white] = nums[white], nums[blue]
+                nums[white], nums[blue] = nums[blue], nums[white]
                 blue -= 1
-        return nums 
+        return nums
