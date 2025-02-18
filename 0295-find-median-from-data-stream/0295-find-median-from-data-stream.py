46133de384e05aff45427b4d1b93380fd632ff45
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-02-17 19:13:48 -0800

    Time: 139 ms (59.8%), Space: 39.7 MB (36.47%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -1,28 +1,27 @@
 class MedianFinder:
+
     def __init__(self):
         self.small, self.large = [], []
 
     def addNum(self, num: int) -> None:
         heapq.heappush(self.small, -1 * num)
 
-        # if small is greater than large
         if self.small and self.large and -1 * self.small[0] > self.large[0]:
             heapq.heappush(self.large, -1 * heapq.heappop(self.small))
         
-        # uneven size
         if len(self.small) > len(self.large) + 1:
             heapq.heappush(self.large, -1 * heapq.heappop(self.small))
-
+        
         if len(self.large) > len(self.small) + 1:
             heapq.heappush(self.small, -1 * heapq.heappop(self.large))
 
     def findMedian(self) -> float:
         if len(self.small) == len(self.large):
-            return (-1 * self.small[0] + self.large[0])/2
+            return (-1 * self.small[0] + self.large[0]) / 2
         
         elif len(self.small) > len(self.large):
             return -1 * self.small[0]
-
+        
         elif len(self.large) > len(self.small):
             return self.large[0]
 
