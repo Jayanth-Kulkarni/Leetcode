54fa1690d61e442fcab016e3eb2b13054859ab9e
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-02-19 16:02:11 -0800

    Time: 968 ms (19.56%), Space: 20.8 MB (39.29%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -9,14 +9,17 @@ class Solution:
             l = i+1
             r = len(nums)-1
             while l < r < len(nums):
-                if first + nums[l] + nums[r] == 0:
-                    res.append([first, nums[l], nums[r]])
-                    l+=1
-                    while l < r < len(nums) and nums[l-1] == nums[l]:
-                        l+=1
-                elif l < r < len(nums) and first + nums[l] + nums[r] > 0:
-                    r -= 1
-                elif l < r < len(nums):
+                second = nums[l]
+                third = nums[r]
+                if first + second + third == 0:
+                    res.append([first, second, third])
                     l += 1
-            
+                    while 0 <= l < r < len(nums) and nums[l-1] == nums[l]:
+                        l += 1
+                
+                elif first + second + third > 0:
+                    r -= 1
+                
+                else:
+                    l += 1
         return res
