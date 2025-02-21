ce9e9041a567209727cbebfb5f18870580040752
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-02-20 16:22:26 -0800

    Time: 0 ms (100%), Space: 19.8 MB (41.53%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -2,7 +2,7 @@ class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         res = []
         for i in range(len(intervals)):
-            # no overlap + result already appended
+            # if end of cur_interval is lesser than new interval's beginning
             if newInterval[1] < intervals[i][0]:
                 res.append(newInterval)
                 return res + intervals[i:]
@@ -11,5 +11,4 @@ class Solution:
             else:
                 newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
         res.append(newInterval)
-    
         return res
