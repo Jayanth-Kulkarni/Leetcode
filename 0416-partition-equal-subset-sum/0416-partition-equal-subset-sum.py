cd4409a91b89c410addd192e929e42b93ebf6021
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-02-20 18:59:49 -0800

    Time: 311 ms (83.93%), Space: 19.2 MB (63.98%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -3,16 +3,16 @@ class Solution:
         if sum(nums) % 2:
             return False
         
-        target = sum(nums)/2
+        target = sum(nums) / 2
         dp = set()
         dp.add(0)
+
         for num in nums:
             copy = set()
-            for ele in dp:
-                copy.add(ele+num)
-                copy.add(ele)
+            for i in dp:
+                copy.add(i)
+                copy.add(i+num)
             dp = copy
-            if target in dp:
+            if target in copy:
                 return True
         return False
-            
