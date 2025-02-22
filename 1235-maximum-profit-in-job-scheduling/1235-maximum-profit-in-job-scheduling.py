30797ba0cc56bd93c6c0bfa2f9f91a35126b8296
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-02-21 21:34:25 -0800

    Time: 199 ms (57.16%), Space: 66.3 MB (16.5%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -3,27 +3,20 @@ class Solution:
         jobs = sorted(zip(startTime, endTime, profit))
         memo = {}
 
-        def find_max_profit(job_id):
-            if job_id == len(jobs):
+        def recurse(id):
+            if id == len(jobs):
                 return 0
-            
-            if job_id in memo:
-                return memo[job_id]
+            if id in memo:
+                return memo[id]
 
-            # get cur job details
-            cur_st, cur_et, cur_p = jobs[job_id]
-            
-            # option 1: skip the current job
-            profit_without_current = find_max_profit(job_id + 1)
-            
-            # option 2: find the next available job and find max profit
-            next_job_id = bisect.bisect(jobs, (cur_et, -1, -1))
-            profit_with_current = cur_p + find_max_profit(next_job_id)
-            
-            # calculate max profit
-            max_profit = max(profit_with_current, profit_without_current)
-            memo[job_id] = max_profit
+            cur_st, cur_et, cur_p = jobs[id]
+            p_wo_id = recurse(id+1)
 
-            return max_profit
+            next_id = bisect.bisect(jobs, (cur_et, -1, -1))
+            p_w_id = cur_p + recurse(next_id)
+
+            memo[id] = max(p_wo_id, p_w_id)
+
+            return memo[id]
         
-        return find_max_profit(0)
+        return recurse(0)
