9074033407a32c01ee3d0e48fa268b6c849a2783
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-02-21 21:55:01 -0800

    Time: 11 ms (16.52%), Space: 19.3 MB (42.65%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,34 +1,30 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        map = {i:[] for i in range(numCourses)}
-
+        map = defaultdict(list)
         for course, prereq in prerequisites:
             map[course].append(prereq)
-
+        
         visited = set()
 
         def dfs(course):
             if course in visited:
                 return False
-
+            
             if map[course] == []:
                 return True
             
             visited.add(course)
-
+            
             for prereq in map[course]:
-                if not dfs(prereq):
+                if dfs(prereq) == False:
                     return False
             
             visited.remove(course)
 
             map[course] = []
-            
-            return True
         
-
         for course in range(numCourses):
-            if not dfs(course):
+            if dfs(course) == False:
                 return False
         
         return True
