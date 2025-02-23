eb844bd8f2007211f9b125089a23c33bdbf39675
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-02-23 10:12:50 -0800

    Time: 195 ms (59%), Space: 66.6 MB (14.69%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -1,22 +1,24 @@
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
         jobs = sorted(zip(startTime, endTime, profit))
+        max_profit = 0
         memo = {}
-
-        def recurse(id):
-            if id == len(jobs):
+        def find_max(i):
+            if i in memo:
+                return memo[i]
+            if i >= len(jobs):
                 return 0
-            if id in memo:
-                return memo[id]
+            # skip to next job
+            p_wo_i = find_max(i+1)
 
-            cur_st, cur_et, cur_p = jobs[id]
-            p_wo_id = recurse(id+1)
+            s,e,p = jobs[i]
+            
+            # find next job with current job
+            n_i = bisect.bisect(jobs, (e, -1, -1))
+            p_w_i = find_max(n_i)
 
-            next_id = bisect.bisect(jobs, (cur_et, -1, -1))
-            p_w_id = cur_p + recurse(next_id)
+            max_profit = max(p_wo_i, p + p_w_i)
+            memo[i] = max_profit
+            return memo[i]
 
-            memo[id] = max(p_wo_id, p_w_id)
-
-            return memo[id]
-        
-        return recurse(0)
+        return find_max(0)
