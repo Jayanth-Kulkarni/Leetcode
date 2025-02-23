77fc22eb6be781f37f921f090ff4ec10b41738b1
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-02-23 12:49:15 -0800

    Time: 83 ms (64.67%), Space: 25.5 MB (12.58%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,26 +1,24 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        d = defaultdict(list)
         q = deque()
         q.append(beginWord)
+        d = defaultdict(list)
         for word in wordList:
             for i in range(len(word)):
-                s = word[:i] + "*" + word[i+1:]
-                d[s].append(word)
+                pattern = word[:i] + "*" + word[i+1:]
+                d[pattern].append(word)
         res = 1
         visited = set()
-        visited.add(beginWord)
         while q:
             for _ in range(len(q)):
                 word = q.popleft()
                 if word == endWord:
                     return res
                 for i in range(len(word)):
-                    s = word[:i] + "*" + word[i+1:]
-                    for nei in d[s]:
+                    pattern = word[:i] + "*" + word[i+1:]
+                    for nei in d[pattern]:
                         if nei not in visited:
                             q.append(nei)
                             visited.add(nei)
-            res += 1
-        
+            res += 1        
         return 0
