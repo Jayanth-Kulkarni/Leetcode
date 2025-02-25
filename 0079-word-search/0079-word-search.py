fde614b7953540f251bb604d7dd6cdbb0a9b60ed
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-02-25 15:52:25 -0800

    Time: 4400 ms (52.43%), Space: 17.8 MB (64.61%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -4,20 +4,23 @@ class Solution:
         visited = set()
 
         def dfs(r, c, i):
-            if i==len(word):
+            if i == len(word):
                 return True
-            if r<0 or c<0 or r>=row or c>=col or (r,c) in visited or board[r][c]!=word[i]:
-                return False
-            
+            if r >= row or r < 0 or c >= col or c < 0 or board[r][c] != word[i] or (r,c) in visited:
+                return False 
+
             visited.add((r,c))
-            res = (dfs(r+1, c, i+1) or
-                dfs(r-1,c,i+1) or
-                dfs(r,c+1,i+1) or
-                dfs(r,c-1,i+1))
+            res = (dfs(r+1, c, i+1) or 
+                dfs(r-1, c, i+1) or
+                dfs(r, c+1, i+1) or
+                dfs(r,c-1, i+1))
             visited.remove((r,c))
-            # print(visited)
+
             return res
-        for i in range(row):
-            for j in range(col):
-                if dfs(i,j,0): return True
+        
+        for r in range(row):
+            for c in range(col):
+                if dfs(r,c,0) == True:
+                    return True
+        
         return False
