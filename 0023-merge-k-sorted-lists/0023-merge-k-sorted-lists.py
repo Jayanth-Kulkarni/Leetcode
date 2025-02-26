f29efa0c4ca4c44327b386d6301feb3aee1b57b6
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-02-25 19:10:09 -0800

    Time: 7 ms (91.46%), Space: 20.2 MB (51.6%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -5,17 +5,17 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists and len(lists) == 0:
+        if len(lists) == 0 or not lists:
             return None
         while len(lists) > 1:
-            mergedlists = []
+            mergelists = []
             for i in range(0,len(lists), 2):
-                l1, l2 = lists[i], lists[i+1] if len(lists) > i+1 else None
-                mergedlists.append(self.merge(l1, l2))
-            lists = mergedlists
+                l1, l2 = lists[i], lists[i+1] if i+1 < len(lists) else None
+                mergelists.append(self.merge(l1,l2))
+            lists = mergelists
         
         return lists[0]
-        
+
     def merge(self, l1, l2):
         head = ListNode()
         tail = head
