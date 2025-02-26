e1c224fc2fa9ade04cd5cb32f34eabfde9c9e09a
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-02-25 18:45:43 -0800

    Time: 49 ms (56.27%), Space: 18.4 MB (58.32%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -2,24 +2,24 @@ class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         if len(p) > len(s):
             return []
-        l, r = 0, len(p)-1
-        res = []
         cur = defaultdict(int)
-        target = defaultdict(int)
-        for i in range(r+1):
-            target[p[i]] += 1
-        for i in range(r+1):
+        tar = defaultdict(int)
+        res = []
+        for i in range(len(p)):
             cur[s[i]] += 1
-        if cur == target:
+        for i in range(len(p)):
+            tar[p[i]] += 1
+        if cur == tar:
             res.append(0)
-        r += 1
-        while 0 <= l < r < len(s):
+        
+        l, r = 0, len(p)
+        while l < r < len(s):
             cur[s[l]] -= 1
             if cur[s[l]] == 0:
                 del cur[s[l]]
             l += 1
             cur[s[r]] += 1
-            if cur == target:
+            if cur == tar:
                 res.append(l)
-            r+=1
+            r += 1
         return res
