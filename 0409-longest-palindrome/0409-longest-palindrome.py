cf23f73a16d8b13885df12df7ee20b56e35de08a
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-02-26 21:08:13 -0800

    Time: 4 ms (14.32%), Space: 17.9 MB (27.58%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,14 +1,13 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
         d = defaultdict(int)
+        res = 0
         for i in s:
             d[i] += 1
-        res = 0
         flag = 0
         for i in d:
             if d[i] % 2 == 0:
                 res += d[i]
-            
             else:
                 flag = 1
                 res += d[i] - 1
