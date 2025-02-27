fd05580d9e45ca3acdfc7907bfe1f08e5c53e70f
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-02-26 22:06:02 -0800

    Time: 4 ms (73.94%), Space: 20.8 MB (82.79%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -15,8 +15,6 @@ class Solution:
             r = dfs(root.right)
 
             self.res = max(self.res, l+r)
-
-            return max(l, r) + 1
-        
+            return max(l,r) + 1
         dfs(root)
         return self.res
