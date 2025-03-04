a247552b581bbf98c4e6a740316cfa43dfa1a62b
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-03-04 14:21:16 -0800

    Time: 0 ms (100%), Space: 17.9 MB (65%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,18 +1,14 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
-        if nums == []:
-            return [[]]
-        def recurse(i,cur):
-            if i >= len(nums):
+        def dfs(i, cur):
+            if i == len(nums):
                 res.append(cur[:])
-                return
-            
+                return 
             cur.append(nums[i])
-            recurse(i+1, cur)
-            cur.pop()
-            recurse(i+1, cur)
-
-            return
-        recurse(0,[])
+            dfs(i+1, cur)
+            cur.remove(nums[i])
+            dfs(i+1, cur)
+        
+        dfs(0, [])
         return res
