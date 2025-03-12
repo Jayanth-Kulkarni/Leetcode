1987d682f9a1a7a1140a56735d27b0e4fb08240b
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-03-11 21:09:46 -0700

    Time: 0 ms (100%), Space: 17.9 MB (41.64%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,24 +1,23 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        dc = {
-            "2":"abc",
-            "3":"def",
-            "4":"ghi",
-            "5":"jkl",
-            "6":"mno",
-            "7":"pqrs",
-            "8":"tuv",
-            "9":"wxyz"
-        }
+        dc = {"2":"abc",
+        "3":"def",
+        "4":"ghi",
+        "5":"jkl",
+        "6":"mno",
+        "7":"pqrs",
+        "8":"tuv",
+        "9":"wxyz"}
+
         res = []
         def recurse(i, cur):
             if i == len(digits):
                 res.append(cur[:])
                 return res
-            
+
             for c in dc[digits[i]]:
                 recurse(i+1, cur+c)
-        
+            
         if digits == "":
             return []
         
