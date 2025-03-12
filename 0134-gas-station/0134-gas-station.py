de5c58fba8dfbc6a17e36ce6a96dfc01f5664c80
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-03-11 20:43:42 -0700

    Time: 24 ms (55.81%), Space: 23.6 MB (15.63%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,11 +1,12 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        total = 0
+        start = 0
+        total_array = []
+        
         if sum(gas) - sum(cost) < 0:
             return -1
         
-        total = 0
-        total_array = []
-        start = 0
         for idx, (i, j) in enumerate(zip(gas, cost)):
             total_array.append(i-j)
             total += i-j
