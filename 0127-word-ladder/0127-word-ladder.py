3ec26af52c41dd433a9ad41c3ddc47932b685bbc
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-03-11 21:50:54 -0700

    Time: 81 ms (66.15%), Space: 25.5 MB (11.9%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,26 +1,25 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        queue = deque()
-        queue.append(beginWord)
-        dc = defaultdict(list)
-        for word in wordList:
-            for i in range(len(word)):
-                s = word[:i] + "*" + word[i+1:]
-                dc[s].append(word)
-        res = 1
+        q = deque()
+        q.append(beginWord)
+        d = defaultdict(list)
         visited = set()
-        visited.add(beginWord)
-        while queue:
-            for _ in range(len(queue)):
-                word = queue.popleft()
+        res = 1
+        for word in wordList:
+            for i in range(0,len(word)):
+                pat = word[:i] + "*" + word[i+1:]
+                d[pat].append(word)
+        while q:
+            for i in range(len(q)):
+                word = q.popleft()
                 if word == endWord:
                     return res
-                for i in range(len(word)):
-                    s = word[:i] + "*" + word[i+1:]
-                    for sub in dc[s]:
-                        if sub not in visited:
-                            queue.append(sub)
-                            visited.add(sub)
-            res += 1
-        
+                for i in range(0, len(word)):
+                    pat = word[:i] + "*" + word[i+1:]
+                    for p in d[pat]:
+                        if p not in visited:
+                            q.append(p)
+                            visited.add(p)
+            if len(q) > 0:
+                res += 1
         return 0
