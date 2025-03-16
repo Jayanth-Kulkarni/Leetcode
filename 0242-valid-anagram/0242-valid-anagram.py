036d49fa6f91c2e4b850d13b2f3a97cef06c9328
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-03-16 13:29:14 -0700

    Time: 10 ms (75.43%), Space: 17.9 MB (64.5%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,18 +1,9 @@
 class Solution:
-    from collections import defaultdict
     def isAnagram(self, s: str, t: str) -> bool:
-        s_d, t_d = defaultdict(int), defaultdict(int)
+        ds, dt = defaultdict(int), defaultdict(int)
         for i in s:
-            s_d[i] += 1
+            ds[i] += 1
         for i in t:
-            t_d[i] += 1
-
-        if len(s_d) != len(t_d):
-            return False
-
-        for key, value in s_d.items():
-            if key in t_d and value == t_d[key]:
-                continue
-            else:
-                return False
-        return True
+            dt[i] += 1
+        
+        return ds == dt
