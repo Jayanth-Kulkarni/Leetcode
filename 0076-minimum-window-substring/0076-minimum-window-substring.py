942cb2e449f8761537747ab9fb6730151d4394da
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-03-20 18:18:19 -0700

    Time: 123 ms (95.54%), Space: 18.2 MB (77.94%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,33 +1,30 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        td, sd = defaultdict(int), defaultdict(int)
+        l, dt, ds = 0, defaultdict(int), defaultdict(int)
+        reslen, res = float("inf"), ""
         for i in t:
-            td[i] += 1
-        l, r = 0, 0
-        reslen = float("inf")
-        res = [-1,-1]
-        want, have = len(td), 0
+            dt[i] += 1
+        have, want = 0, len(dt)
         for r in range(len(s)):
             cur = s[r]
-            sd[cur] += 1
-            if cur in td and sd[cur] == td[cur]:
+            ds[cur] += 1
+
+            if cur in dt and ds[cur] == dt[cur]:
                 have += 1
-            while want == have:
+            
+            while have == want:
                 if reslen > r-l+1:
                     reslen = r-l+1
                     res = s[l:r+1]
-
-                cl = s[l]
-                sd[cl] -= 1
                 
-                if sd[cl] == 0:
-                    del sd[cl]
+                curl = s[l]
+                ds[curl] -= 1
                 
-                if cl in td and sd[cl] < td[cl]:
+                if ds[curl] == 0:
+                    del ds[curl]
+                
+                if curl in dt and ds[curl] < dt[curl]:
                     have -= 1
+                
                 l += 1
-            
-        if reslen == float("inf"):
-            return ""
-        
         return res
