5a1182762bd2b21ab4ceed019380e841c8c7080e
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-04-22 21:53:03 -0700

    Time: 260 ms (42.19%), Space: 25.6 MB (17.94%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -5,21 +5,22 @@ class Solution:
         islands = 0
         def dfs(r, c):
             queue = deque()
-            queue.append((r,c))
-            visited.add((r,c))
+            queue.append((r, c))
+            visited.add((r, c))
             while queue:
-                ro,co = queue.popleft()
-                for r_, c_ in [(1,0), (-1,0), (0,1), (0,-1)]:
-                    r1 = ro + r_
-                    c1 = co + c_
-                    if 0 <= r1 < row and 0 <= c1 < col and (r1,c1) not in visited and grid[r1][c1] == "1":
-                        visited.add((r1,c1))
-                        queue.append((r1,c1))
-
+                ro, co = queue.popleft()
+                for r_, c_ in [(1,0), (-1, 0), (0, 1), (0, -1)]:
+                    r1 = r_ + ro
+                    c1 = c_ + co
+                    if 0 <= r1 < row and 0 <= c1 < col and (r1, c1) not in visited and grid[r1][c1] == "1":
+                        visited.add((r1, c1))
+                        queue.append((r1, c1))
+        
         for r in range(row):
             for c in range(col):
-                if grid[r][c] == "1" and (r,c) not in visited:
-                    dfs(r,c)
+                if grid[r][c] == "1" and (r, c) not in visited:
+                    dfs(r, c)
                     islands += 1
+                    
         
         return islands
