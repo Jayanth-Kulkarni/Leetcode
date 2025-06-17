0b6cc3ff6b0871896e70a5468c92b9776e925c2a
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-06-17 16:28:18 -0700

    Time: 63 ms (82.12%), Space: 18.3 MB (54.52%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,30 +1,28 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        l, dt, ds = 0, defaultdict(int), defaultdict(int)
-        reslen, res = float("inf"), ""
-        for i in t:
-            dt[i] += 1
-        have, want = 0, len(dt)
+        if t == "": return ""
+        countT, window = {}, {}
+        for c in t:
+            countT[c] = 1 + countT.get(c, 0)
+        
+        have, need = 0, len(countT)
+        res, reslen = [-1, -1], float("inf")
+        l = 0
         for r in range(len(s)):
-            cur = s[r]
-            ds[cur] += 1
+            c = s[r]
+            window[c] = 1 + window.get(c, 0)
 
-            if cur in dt and ds[cur] == dt[cur]:
+            if c in countT and window[c] == countT[c]:
                 have += 1
             
-            while have == want:
-                if reslen > r-l+1:
+            while have == need:
+                if (r-l+1) < reslen:
                     reslen = r-l+1
-                    res = s[l:r+1]
+                    res = [l, r]
                 
-                curl = s[l]
-                ds[curl] -= 1
-                
-                if ds[curl] == 0:
-                    del ds[curl]
-                
-                if curl in dt and ds[curl] < dt[curl]:
+                window[s[l]] -= 1
+                if s[l] in countT and window[s[l]] < countT[s[l]]:
                     have -= 1
-                
                 l += 1
-        return res
+        l, r = res
+        return s[l:r+1] if reslen != float("inf") else ""
