ae2ba8200c457d4528d97b477ad5793a2bf5ad67
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-06-25 20:03:44 -0700

    Time: 754 ms (27.29%), Space: 20.7 MB (68.75%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,21 +1,25 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums = sorted(nums)
+        """
+        Loop over all the nums and call that as the first element
+        Perform 2 pointer search on the remaining array and see if the sum = 0
+        """
         res = []
-        for i in range(len(nums)):
-            if i >0 and nums[i-1] == nums[i]:
-                continue
-            l = i + 1
-            r = len(nums)-1
-            while 0 <= l < r:
-                f, s, t = nums[i], nums[l], nums[r]
-                if f + s + t == 0:
-                    res.append([f, s, t])
-                    l += 1
-                    while 0 <= l <= r and nums[l] == nums[l-1]:
-                        l+= 1
-                elif f + s + t > 0:
-                    r -= 1
+        nums.sort()
+        for fid in range(len(nums)):
+            f = nums[fid]
+            if fid > 0 and nums[fid-1] == nums[fid]:
+                continue 
+            sid, tid = fid+1, len(nums)-1
+            while sid < tid:
+                if nums[sid] + nums[tid] + f == 0:
+                    res.append([nums[sid], nums[tid], f]) 
+                    sid += 1
+                    tid -= 1
+                    while sid < len(nums)  and nums[sid] == nums[sid-1]:
+                        sid += 1
+                elif nums[sid] + nums[tid] + f > 0:
+                    tid -= 1
                 else:
-                    l += 1
+                    sid += 1
         return res
