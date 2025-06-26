06260f94b3b00aee1a530510da7a8d5f0e1e40c5
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-06-25 19:42:53 -0700

    Time: 43 ms (86.01%), Space: 19.8 MB (57.03%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -6,10 +6,15 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
+        """
+        have 2 pointers, 1 moving at twice the speed as the other
+        see if they meet, if yes return true; otherwise false
+        """
         slow, fast = head, head
-        while slow and fast and fast.next:
+        while slow and slow.next and fast and fast.next:
             slow = slow.next
             fast = fast.next.next
             if slow == fast:
                 return True
+        
         return False
