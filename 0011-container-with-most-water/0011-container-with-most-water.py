f2873e6601e94e5b168df2b752530bc6afc8dc2b
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-06-25 19:10:34 -0700

    Time: 115 ms (11.73%), Space: 28.4 MB (67.37%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,11 +1,18 @@
+"""
+initialize left, right to 0 and lenght
+If find area, compare with max and reset max if needed
+if left is < right, move left pointer by 1 otherwise right by 1
+Keep finding the max area while left < right
+return max area
+"""
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         left, right = 0, len(height)-1
-        maxarea = 0
-        for i in range(len(height)):
-            maxarea = max(maxarea, min(height[left], height[right])* (right - left))
-            if height[left] > height[right]:
-                right -= 1
-            else:
+        max_area = 0
+        while left < right:
+            max_area = max(max_area,(right - left) * min(height[left], height[right]))
+            if height[left] < height[right]:
                 left += 1
-        return maxarea
+            else:
+                right -= 1
+        return max_area
