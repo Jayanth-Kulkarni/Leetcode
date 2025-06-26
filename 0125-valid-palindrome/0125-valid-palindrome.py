e198865afb9f223c455966454e39b262ecbbf55a
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-06-25 19:33:02 -0700

    Time: 3 ms (98.96%), Space: 18 MB (83.21%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,8 +1,23 @@
+
+import string
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = s.lstrip().replace(" ","")
-        for i in string.punctuation:
-            s = s.replace(i, "")
+        """
+        Use string library to remove all the special characters
+        Remove all the spaces and convert the string to lowercase
+        Start a left and right pointer at beginning and end of the string
+        Compare both and keep moving them until left < right
+        if they are not the same return false
+        return true in the end
+        """
+        for special in string.punctuation:
+            s = s.replace(special, "")
+        s = s.replace(" ", "")
         s = s.lower()
-        print(s)
-        return s == s[::-1]
+        left, right = 0, len(s)-1
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left +=1
+            right -= 1
+        return True
