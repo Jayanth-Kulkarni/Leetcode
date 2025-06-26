b9f3b90822447b75d9ebb95e491dbaf8ae41c009
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-06-26 10:20:00 -0700

    Time: 37 ms (19.81%), Space: 28.1 MB (5.21%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,18 +1,25 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        preprod, postprod = [nums[0]], [nums[-1]]
+        """
+        Process 
+        1. Have a prefix product calculated till the index
+        2. Have a postfix product calculated till the index in reverse order
+        3. If prefix[i-1] and postfix[i+1] exist, then result[i] = prefix[i-1] * postfix[i+1]
+        4. Otherwise result[i] = ?
+        """
+        prefix, postfix = [nums[0]], [nums[-1]]
         for i in nums[1:]:
-            preprod.append(preprod[-1] * i)
-        nums = nums[::-1]
-        for i in nums[1:]:
-            postprod.append(postprod[-1] * i)
-        postprod = postprod[::-1]
-        res = []
+            prefix.append(prefix[-1] * i)
+        reverse = nums[::-1]
+        for i in reverse[1:]:
+            postfix.append(postfix[-1] * i)
+        postfix = postfix[::-1]
+        result = []
         for i in range(len(nums)):
-            if i-1 >= 0 and i + 1 < len(nums):
-                res.append(preprod[i-1] * postprod[i+1])
-            elif i-1 < 0:
-                res.append(postprod[i+1])
-            elif i+1 >= len(nums):
-                res.append(preprod[i-1])
-        return res
+            if i-1 >= 0 and i+1 < len(nums):
+                result.append(prefix[i-1] * postfix[i+1])
+            elif i-1 >= 0:
+                result.append(prefix[i-1])
+            elif i+1 < len(nums):
+                result.append(postfix[i+1])
+        return result
