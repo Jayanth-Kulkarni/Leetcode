1ffe5327d2eb0fa22b854e5283c8df49c204b492
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-06-26 13:28:34 -0700

    Time: 27 ms (28.6%), Space: 19.6 MB (12.08%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,18 +1,19 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        premax = [0]
-        postmax = [0]
+        """
+        Find a min list and max list 
+        """
+        pre_max_list = [0]
+        post_max_list = [0]
+        result = 0
         for i in height:
-            premax.append(max(premax[-1], i))
-        
+            pre_max_list.append(max(i, pre_max_list[-1]))
         for i in height[::-1]:
-            postmax.append(max(postmax[-1], i))
+            post_max_list.append(max(i, post_max_list[-1]))
+
+        post_max_list = post_max_list[::-1]
+        for i,j,k in zip(height,pre_max_list,post_max_list):
+            if min(j,k) - i > 0:
+                result += min(j,k) - i
         
-        postmax = postmax[::-1]
-        print(premax, postmax)
-        res = 0
-        for i, j, h in zip(premax, postmax, height):
-            if min(i,j) - h > 0:
-                res += min(i,j) - h
-        
-        return res
+        return result
