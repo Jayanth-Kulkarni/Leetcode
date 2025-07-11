6b88ca03e863f35623b8833232d52578c5804bfc
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-07-11 11:37:02 -0700

    Time: 19 ms (66.39%), Space: 17.7 MB (97.72%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,21 +1,18 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2):
-            return False
-        target = defaultdict(int)
-        window_dict = defaultdict(int)
-        for idx, i in enumerate(s1):
-            target[i] += 1
-            window_dict[s2[idx]] += 1
+        target_dict = defaultdict(int)
+        current_dict = defaultdict(int)
+        for i in s1:
+            target_dict[i] += 1
         l = 0
-        if target == window_dict:
-            return True
-        for r in range(len(s1), len(s2)):
-            window_dict[s2[r]] += 1
-            window_dict[s2[l]] -= 1
-            if window_dict[s2[l]] == 0:
-                del window_dict[s2[l]]
-            if target == window_dict:
-                return True
-            l += 1
+        for r in range(len(s2)):
+            current_dict[s2[r]] += 1
+            if r-l+1 == len(s1):
+                if current_dict == target_dict:
+                    return True
+                current_dict[s2[l]] -= 1
+                if current_dict[s2[l]] == 0:
+                    del current_dict[s2[l]]
+                l+=1
+        
         return False
