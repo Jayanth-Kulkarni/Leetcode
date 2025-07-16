dff3e49f66818752dacb62a428502fdeb9577511
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-07-16 10:50:11 -0700

    Time: 408 ms (92.98%), Space: 18 MB (12.38%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.py
+++ 0611-valid-triangle-number/0611-valid-triangle-number.py
@@ -1,19 +1,14 @@
 class Solution:
     def triangleNumber(self, nums: List[int]) -> int:
-        """
-        1. Sort them
-        2. Have the last pointer to the end
-        3. 
-        """
-
         nums.sort()
-        count = 0
-        for current_biggest in range(len(nums)-1, -1, -1):
-            l, r = 0, current_biggest - 1
+        res = 0
+        for last in range(len(nums)-1, -1, -1):
+            biggest = nums[last]
+            l, r = 0, last-1
             while l < r:
-                if nums[l] + nums[r] > nums[current_biggest]:
-                    count +=  r - l
+                if nums[l] + nums[r] > biggest:
+                    res += r-l
                     r -= 1
                 else:
                     l += 1
-        return count
+        return res
