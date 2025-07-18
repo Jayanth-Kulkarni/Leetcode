71f0efb030ce31561c9d4e0a761824387bf332ce
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-07-18 11:18:05 -0700

    Time: 0 ms (100%), Space: 17.8 MB (33.28%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,14 +1,17 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        red, white, blue = 0, 0, len(nums) - 1
-        while white <= blue:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        red, white, blue = 0, 0, len(nums)-1
+        while 0 <= red <= white <= blue < len(nums):
             if nums[white] == 0:
                 nums[white], nums[red] = nums[red], nums[white]
-                red += 1
                 white += 1
+                red += 1
             elif nums[white] == 1:
                 white += 1
-            elif nums[white] == 2:
+            else:
                 nums[white], nums[blue] = nums[blue], nums[white]
                 blue -= 1
-        
+        return
