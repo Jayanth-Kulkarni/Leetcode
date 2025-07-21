f6b7b97cb2d8ea1b045755a03d3edb27e3ffab16
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-07-21 13:40:11 -0700

    Time: 423 ms (70.23%), Space: 17.9 MB (53.45%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.py
+++ 0611-valid-triangle-number/0611-valid-triangle-number.py
@@ -2,12 +2,11 @@ class Solution:
     def triangleNumber(self, nums: List[int]) -> int:
         nums.sort()
         res = 0
-        for last in range(len(nums)-1, -1, -1):
-            biggest = nums[last]
-            l, r = 0, last-1
+        for cur in range(len(nums)-1, -1, -1):
+            l, r = 0, cur-1
             while l < r:
-                if nums[l] + nums[r] > biggest:
-                    res += r-l
+                if nums[l] + nums[r] > nums[cur]:
+                    res += (r - l)
                     r -= 1
                 else:
                     l += 1
