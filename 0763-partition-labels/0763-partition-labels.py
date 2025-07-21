78b93e59b6b2d1d33765152fe0f2db707deb7f9b
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-07-21 13:15:27 -0700

    Time: 20 ms (5.4%), Space: 18 MB (12.29%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -1,14 +1,14 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        last = defaultdict(int)
+        d = defaultdict(int)
         for idx, i in enumerate(s):
-            last[i] = idx
+            d[i] = idx
         start = end = 0
         result = []
         for i, char in enumerate(s):
-            end = max(last[char], end)
-
+            end = max(d[char], end)
+            print(char, end)
             if i == end:
                 result.append(end - start + 1)
-                start = i+1
+                start = i + 1
         return result
