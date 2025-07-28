427bc1f1a89dc2fd3dbee33f6cb570dc4a351204
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-07-28 16:08:42 -0700

    Time: 3 ms (40.46%), Space: 17.9 MB (18.41%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,17 +1,15 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        def solve(num):
-            new_num = 0
-            while num:
-                last_digit = num % 10
-                num = num // 10
-                new_num += last_digit * last_digit
-            return new_num
-        slow, fast = solve(n), solve(solve(n))
-        while slow != fast:
-            slow, fast = solve(slow), solve(solve(fast))
+        def findHappy(n):
+            res = 0
+            while n:
+                res += (n % 10) * (n % 10)
+                n = n // 10
+            return res
+        
+        slow, fast = findHappy(n), findHappy(findHappy(n))
+        while fast != slow:
+            slow, fast = findHappy(slow), findHappy(findHappy(fast))
             if slow == 1 or fast == 1:
                 return True
-        if slow == 1 or fast == 1:
-            return True
-        return False
+        return slow == 1
