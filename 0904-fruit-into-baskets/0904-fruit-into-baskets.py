6719a942d8a1caa41aafb1024fe30121eab371b3
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-08-05 09:03:56 -0700

    Time: 175 ms (74.24%), Space: 23.8 MB (16.47%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,17 +1,13 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        """
-        move from left to right and keep collecting, as soon as the dict > 3, start removing elements from left
-        """
-        l, r, res = 0, 0, 0
-        d = defaultdict(int)
-        while l <= r < len(fruits):
-            d[fruits[r]] += 1
-            while len(d) > 2:
-                d[fruits[l]] -= 1
-                if d[fruits[l]] == 0:
-                    del d[fruits[l]]
+        res = defaultdict(int)
+        max_len = l =  0
+        for r in range(len(fruits)):
+            res[fruits[r]] += 1
+            while len(res) > 2:
+                res[fruits[l]] -= 1
+                if res[fruits[l]] == 0:
+                    del res[fruits[l]]
                 l += 1
-            res = max(res, r-l+1)
-            r += 1
-        return res
+            max_len = max(max_len, r-l+1)
+        return max_len
