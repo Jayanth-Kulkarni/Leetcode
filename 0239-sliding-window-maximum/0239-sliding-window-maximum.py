11831ed9c00db2aae113d90ec591261e624c07e6
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-08-07 12:23:16 -0700

    Time: 160 ms (69.39%), Space: 31.1 MB (79.6%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,14 +1,9 @@
-from collections import deque
-from typing import List
-
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        # Monotonic deque: holds indices of elements in decreasing order of their values
         window = deque()
-        output = []
-
+        res = []
+        l = 0
         for r in range(len(nums)):
-            # Remove elements from the back that are smaller than the current element
             while window and nums[window[-1]] < nums[r]:
                 window.pop()
             window.append(r)
@@ -19,6 +14,7 @@ class Solution:
 
             # Once we have the first full window, start recording max values
             if r >= k - 1:
-                output.append(nums[window[0]])
+                res.append(nums[window[0]])
 
-        return output
+
+        return res
