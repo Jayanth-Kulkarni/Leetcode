d5c3d1f4464e2cd95674187eee5405290d149b97
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-08-10 17:16:43 -0700

    Time: 254 ms (17.47%), Space: 18.4 MB (15.72%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,21 +1,22 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        wd = defaultdict(int)
-        hd = defaultdict(int)
-        l, want, have, reslen, res = 0, 0, 0, float("inf"), [-1,-1]
-        for i in t:
-            wd[i] += 1
+        want, have, l = 0, 0, 0
+        reslen = float("inf")
+        res = [-1, -1]
+        wd, hd = defaultdict(int), defaultdict(int)
+        for char in t:
+            wd[char] += 1
         want = len(wd)
         for r in range(len(s)):
             cur = s[r]
             hd[cur] += 1
             if cur in wd and hd[cur] == wd[cur]:
                 have += 1
+            print(cur, have, reslen, res)
             while have == want:
-                if r-l+1 < reslen:
+                if reslen > r-l+1:
                     reslen = r-l+1
                     res = [l, r]
-                
                 left = s[l]
                 hd[left] -= 1
                 if left in wd and hd[left] < wd[left]:
@@ -23,6 +24,5 @@ class Solution:
                 l += 1
         
         l, r = res
-        if reslen == float("inf"):
-            return ""
-        return s[l:r+1]
+        return s[l:r+1] if reslen != float("inf") else ""
+        
