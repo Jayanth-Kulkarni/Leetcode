eb53654a6407beb128105ee35cb932ec333b0da3
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-09-29 21:35:09 -0700

    Time: 128 ms (27.44%), Space: 26.7 MB (94.7%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -3,6 +3,7 @@ class Solution:
         min_val = float("inf")
         max_profit = 0
         for price in prices:
-            min_val = min(price, min_val)
-            max_profit = max(max_profit, price - min_val)
+            profit = price - min_val
+            min_val = min(min_val, price)
+            max_profit = max(max_profit, profit)
         return max_profit
