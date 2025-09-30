90a7073f090b410ca91df65ba056d2663b15a92a
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-09-29 17:48:20 -0700

    Time: 185 ms (5.03%), Space: 91.1 MB (5.04%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -6,13 +6,11 @@ class Solution:
         def dfs(num):
             if num not in res:
                 return 0
+            
             if num in memo:
                 return memo[num]
-            
-            count = 1 + dfs(num+1)
-            memo[num] = count
-            return count
-        
+            memo[num] = 1 + dfs(num+1)
+            return memo[num]
         for num in nums:
             max_count = max(max_count, dfs(num))
         
