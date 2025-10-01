86a69f0c0ff05e85f01812d61ac88370fd54318c
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-09-30 17:54:57 -0700

    Time: 19 ms (64.9%), Space: 18.1 MB (19.29%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,17 +1,16 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        target = defaultdict(int)
-        cur = defaultdict(int)
-        l = 0
-        for i in s1:
-            target[i] += 1
+        want, have, l = defaultdict(int), defaultdict(int), 0
+        for chr in s1:
+            want[chr] += 1
         for r in range(len(s2)):
-            cur[s2[r]] += 1
-            if r-l+1 >= len(s1):
-                if cur == target:
-                    return True
-                cur[s2[l]] -= 1
-                if cur[s2[l]] == 0:
-                    del cur[s2[l]]
+            if r-l+1 > len(s1):
+                have[s2[l]] -= 1
+                if have[s2[l]] == 0:
+                    del have[s2[l]]
                 l += 1
+            have[s2[r]] += 1
+            if have == want:
+                return True
         return False
+
