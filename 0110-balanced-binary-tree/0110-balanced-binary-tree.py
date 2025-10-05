50959942d031857d888d6846e3e41f4d98bc0722
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-10-05 15:10:43 -0700

    Time: 7 ms (29.62%), Space: 18.9 MB (62.41%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -10,11 +10,10 @@ class Solution:
             if not root:
                 return [True, 0]
             
-            l = dfs(root.left)
-            r = dfs(root.right)
+            left = dfs(root.left)
+            right = dfs(root.right)
 
-            balanced = True if l[0] and r[0] and abs(l[1] - r[1]) <= 1 else False
-
-            return [balanced, max(l[1], r[1])+1]
-
+            balanced = (left[0] and right[0] and abs(left[1]-right[1]) <= 1)
+            return [balanced, 1 + max(left[1], right[1])]
         return dfs(root)[0]
+        
