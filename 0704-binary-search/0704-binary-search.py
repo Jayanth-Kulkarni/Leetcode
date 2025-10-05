6f316d007fe40b088fa3d34d2375400702af67b6
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-10-05 10:12:13 -0700

    Time: 0 ms (100%), Space: 18.5 MB (94.11%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -5,8 +5,8 @@ class Solution:
             mid = (left + right)//2
             if nums[mid] == target:
                 return mid
-            if nums[mid] < target:
-                left = mid+1
-            else:
+            elif nums[mid] > target:
                 right = mid-1
+            else:
+                left = mid+1
         return -1
