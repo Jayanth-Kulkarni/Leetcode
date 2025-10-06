b3e6165ecce2a12c3e6bc712e4bc8bafffaa28ad
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-10-05 21:37:08 -0700

    Time: 0 ms (100%), Space: 17.7 MB (60.82%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -7,24 +7,22 @@
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         if not root:
-            return []
+            return [] 
         q = deque()
         q.append([root])
         res = [root.val]
         while q:
-            level = q.popleft()
-            inner_level = []
-            inner_res = []
-            for i in range(len(level)):
-                node = level[i]
-                if node.left != None:
-                    inner_level.append(node.left)
-                    inner_res.append(node.left.val)
-                if node.right != None:
-                    inner_level.append(node.right)
-                    inner_res.append(node.right.val)
-
-            if inner_level != []:
-                res.append(inner_res[-1])
-                q.append(inner_level)
+            current_level = q.pop()
+            inner_list = []
+            inner_nodes = []
+            for node in current_level:
+                if node.left:
+                    inner_list.append(node.left.val)
+                    inner_nodes.append(node.left)
+                if node.right:
+                    inner_list.append(node.right.val)
+                    inner_nodes.append(node.right)
+            if inner_list != [] or inner_nodes != []:
+                res.append(inner_list[-1])
+                q.append(inner_nodes)
         return res
