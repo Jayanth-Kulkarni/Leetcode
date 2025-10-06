a83241025f37ecd472db249ec5fc2250c0eda1bf
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-10-06 08:44:47 -0700

    Time: 150 ms (36.54%), Space: 31.9 MB (91.06%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -1,19 +1,27 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        def dfs(root,max_so_far):
-            if not root:
-                return 0
-            count = 1 if root.val >= max_so_far else 0
-            max_so_far = max(max_so_far, root.val)
-            left = dfs(root.left, max_so_far)
-            right = dfs(root.right, max_so_far)
-
-            return left + right + count
-        
-        return dfs(root, float("-inf"))
+        self.count = 0
+        '''
+        This function will update the self.count values if the path from root to X
+        has not nodes with value greater than x, by getting the max_val arg that records 
+        the current max value in the path.
+        input: X, max_val 
+        output: update the count and not return anything
+        '''
+        def dfs(X, max_val):
+            if not X:
+                return
+            if X.val >= max_val:
+                self.count += 1
+            max_val = max(max_val, X.val)
+            dfs(X.left, max_val)
+            dfs(X.right, max_val)
+            return
+        dfs(root, root.val)
+        return self.count
