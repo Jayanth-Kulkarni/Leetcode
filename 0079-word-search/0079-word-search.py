c402b997ab68a0e651df5b6b6f6f37a94d13455c
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-10-06 20:14:52 -0700

    Time: 3953 ms (59.21%), Space: 17.9 MB (65.47%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,21 +1,27 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         row, col = len(board), len(board[0])
-        visited = set()
-        def dfs(i, r, c):
-            if i == len(word):
+        
+        def dfs(r, c, cur):
+            cur += board[r][c][0]
+            if cur != word[:len(cur)] or visited[r][c]:
+                return
+            if cur == word:
                 return True
-            if not (0 <= r < row and 0 <= c < col and board[r][c] == word[i] and (r,c) not in visited):
-                return False
-            visited.add((r,c))
-            res = dfs(i+1, r+1, c) or dfs(i+1, r-1, c) or dfs(i+1, r, c+1) or dfs(i+1, r, c-1)
-            visited.remove((r,c))
-            return res
-       
-        for i in range(row):
-            for j in range(col):
-                if dfs(0, i, j) == True:
+            visited[r][c] = True
+            if r+1 < row:
+                if dfs(r+1,c,cur): return True
+            if r-1 >= 0:
+                if dfs(r-1,c,cur): return True
+            if c+1 < col:
+                if dfs(r,c+1,cur): return True
+            if c-1 >= 0:
+                if dfs(r,c-1,cur): return True
+            visited[r][c] = False
+        visited = [[False for _ in range(col)] for _ in range(row)]
+        for r in range(row):
+            for c in range(col):
+                visited[r][c] = False
+                if dfs(r, c, ""):
                     return True
-        
         return False
-        
