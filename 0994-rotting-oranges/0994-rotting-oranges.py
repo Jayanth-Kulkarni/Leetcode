5d8775ef5670036e91ee23a05e19fd2fdf04b50b
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-10-07 20:58:49 -0700

    Time: 0 ms (100%), Space: 17.7 MB (71.41%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,28 +1,30 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        q = deque()
         row, col = len(grid), len(grid[0])
-        res = 0
+        
+        queue = deque()
+        fresh = 0
+        max_time = 0
         for r in range(row):
             for c in range(col):
                 if grid[r][c] == 2:
-                    q.append((r,c))
+                    queue.append((r, c, 0))
+                elif grid[r][c] == 1:
+                    fresh += 1
         
-        while q:
-            for i in range(len(q)):
-                r_, c_ = q.popleft()
-                for r1, c1 in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-                    r = r_ + r1
-                    c = c_ + c1
-                    if 0 <= r < row and 0 <= c < col and grid[r][c] == 1:
-                        grid[r][c] = 2
-                        q.append((r,c))
-            if len(q) > 0:
-                res+= 1
+        while queue:
+            r, c, t = queue.popleft()
+            max_time = max(t, max_time)
+
+            for r_, c_ in [[1, 0], [-1, 0], [0, 1], [0, -1]]:
+                r1 = r_ + r
+                c1 = c_ + c
+                if 0<= r1 < row and 0<= c1 < col and grid[r1][c1] == 1:
+                    fresh -= 1
+                    grid[r1][c1] = 2
+                    queue.append((r1, c1, t+1))
         
-        for r in range(row):
-            for c in range(col):
-                if grid[r][c] == 1:        
-                    return -1
-        return res
-            
+        if fresh == 0:
+            return max_time
+        else:
+            return -1
