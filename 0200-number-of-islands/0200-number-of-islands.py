e7d96121f2f31a89b70161299837ce69d64521e8
Jayanth Prakash Kulkarni <kulka112@uwindsor.ca>
2025-10-07 19:53:59 -0700

    Time: 260 ms (39.14%), Space: 25.4 MB (26.62%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -3,24 +3,23 @@ class Solution:
         row, col = len(grid), len(grid[0])
         visited = set()
         islands = 0
-        def dfs(r, c):
+        def bfs(r, c):
             queue = deque()
             queue.append((r, c))
-            visited.add((r, c))
+            visited.add((r,c))
             while queue:
                 ro, co = queue.popleft()
-                for r_, c_ in [(1,0), (-1, 0), (0, 1), (0, -1)]:
-                    r1 = r_ + ro
-                    c1 = c_ + co
+                for r_, c_ in [(1,0), (-1,0), (0, 1), (0, -1)]:
+                    r1 = ro + r_
+                    c1 = co + c_
                     if 0 <= r1 < row and 0 <= c1 < col and (r1, c1) not in visited and grid[r1][c1] == "1":
                         visited.add((r1, c1))
                         queue.append((r1, c1))
-        
+
         for r in range(row):
             for c in range(col):
-                if grid[r][c] == "1" and (r, c) not in visited:
-                    dfs(r, c)
+                if grid[r][c] == "1" and (r,c) not in visited:
+                    bfs(r,c)
                     islands += 1
-                    
         
         return islands
